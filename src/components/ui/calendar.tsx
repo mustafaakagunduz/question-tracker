8f4f9a4ba175c9202261d102cd977878326f2ff1
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2025-04-12 22:43:56 +0300

    takvim işlevlendirildi ama şekli kayık

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,17 +2,20 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import { DayPicker, DayPickerProps } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
+
+// React Day Picker'ın güncel versiyonuna göre düzeltme
+type CalendarProps = React.ComponentProps<typeof DayPicker>
 
 function Calendar({
                       className,
                       classNames,
                       showOutsideDays = true,
                       ...props
-                  }: React.ComponentProps<typeof DayPicker>) {
+                  }: CalendarProps) {
     return (
         <DayPicker
             showOutsideDays={showOutsideDays}
@@ -31,7 +34,8 @@ function Calendar({
                 nav_button_next: "absolute right-1",
                 table: "w-full border-collapse space-x-1",
                 head_row: "flex",
-                head_cell: "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
+                head_cell:
+                    "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
                 row: "flex w-full mt-2",
                 cell: cn(
                     "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-range-end)]:rounded-r-md",
@@ -58,15 +62,9 @@ function Calendar({
                 day_hidden: "invisible",
                 ...classNames,
             }}
-            components={
-                {
-                    IconLeft: () => <ChevronLeft className="size-4" />,
-                    IconRight: () => <ChevronRight className="size-4" />,
-                } as any
-            }
 
+            {...props}
         />
-
     )
 }
 
