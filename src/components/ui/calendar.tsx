ced1b93c4a0f82e6446669f45fb2f6e3532cede3
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2025-04-12 22:31:25 +0300

    tarihe göre sort

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -31,8 +31,7 @@ function Calendar({
                 nav_button_next: "absolute right-1",
                 table: "w-full border-collapse space-x-1",
                 head_row: "flex",
-                head_cell:
-                    "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
+                head_cell: "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
                 row: "flex w-full mt-2",
                 cell: cn(
                     "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-range-end)]:rounded-r-md",
@@ -59,16 +58,15 @@ function Calendar({
                 day_hidden: "invisible",
                 ...classNames,
             }}
-            icons={{
-                prev: ({ ...props }) => (
-                    <ChevronLeft className={cn("size-4")} {...props} />
-                ),
-                next: ({ ...props }) => (
-                    <ChevronRight className={cn("size-4")} {...props} />
-                ),
-            }}
-            {...props}
+            components={
+                {
+                    IconLeft: () => <ChevronLeft className="size-4" />,
+                    IconRight: () => <ChevronRight className="size-4" />,
+                } as any
+            }
+
         />
+
     )
 }
 
